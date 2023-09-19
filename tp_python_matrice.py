41b2e9aad3e3c17a417689b206ad5185b325b34b
Esteban AUSSANT <esteban.aussant@tpi10.cpe.lan>
2023-09-19 12:17:45 +0200

    code pas fini

--- tp_python_matrice.py
+++ tp_python_matrice.py
@@ -1,23 +1,26 @@
-M=[[ 1,2 ],
-[ 5,6 ]]
-
-N=[[ 6,2 ],
-   [ 8,2 ],
-   [ 3,1 ]]
-
 
 def multiplication(A,B):
+    '''A et B deux matrices '''
     nA = len(A) ; nB = len(B)
     pA = len(A[0])
-    C=[]
+    C = [[0 for j in range(nB)] for i in range(pA)]
     if nA != len(B[0]) :
         return 'multiplication impossible'
     for i in range (pA):
         for j in range(nB):
             for k in range(nA):
-                C[i:j] += A[i][k] * B[k][i]
+                C[i][j] += A[i][k] * B[k][j]
     return C
 
 
+
+M=[[ 1,2 ],
+[ 5,6 ]]
+
+
+
+N=[[ 6,2 ],
+   [ 8,2 ],
+   [ 3,1 ]]
 print (multiplication(M, N))
 print (multiplication(N,M))
